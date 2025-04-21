d2f9a84d51fbc4df2c316fd3e71b6267e626bc00
stuarttimwhite <stuart.white@spaceandtime.io>
2025-04-21 13:19:26 -0400

    feat: add div-mod constraints

--- crates/proof-of-sql/src/sql/proof_gadgets/mod.rs
+++ crates/proof-of-sql/src/sql/proof_gadgets/mod.rs
@@ -1,5 +1,4 @@
 //! This module contains shared proof logic for multiple `ProofExpr` / `ProofPlan` implementations.
-#[cfg(test)]
 mod divide_and_modulo_expr;
 mod filter_base;
 pub(crate) use filter_base::{final_round_evaluate_filter, verify_evaluate_filter};
