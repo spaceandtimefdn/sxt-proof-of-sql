5835e37e9b3850b54276b4f9e16b1570af50676c
Jacob Trombetta <jacob.trombetta@spaceandtime.io>
2025-05-20 12:17:13 -0400

    refactor: clean up mul_add_assign_tests module

--- crates/proof-of-sql/src/base/slice_ops/mul_add_assign_test.rs
+++ crates/proof-of-sql/src/base/slice_ops/mul_add_assign_test.rs
@@ -3,47 +3,24 @@ use crate::base::scalar::test_scalar::TestScalar;
 
 #[test]
 fn test_mul_add_assign() {
-    let mut a = vec![
-        TestScalar::from(1i32),
-        TestScalar::from(2i32),
-        TestScalar::from(3i32),
-        TestScalar::from(4i32),
-    ];
+    let mut a = [1, 2, 3, 4].map(TestScalar::from).to_vec();
     let b = vec![2, 3, 4, 5];
     mul_add_assign(&mut a, TestScalar::from(10i32), &b);
-    let c = vec![
-        TestScalar::from(1 + 10 * 2),
-        TestScalar::from(2 + 10 * 3),
-        TestScalar::from(3 + 10 * 4),
-        TestScalar::from(4 + 10 * 5),
-    ];
+    let c = [1 + 10 * 2, 2 + 10 * 3, 3 + 10 * 4, 4 + 10 * 5]
+        .map(TestScalar::from)
+        .to_vec();
     assert_eq!(a, c);
 }
 
 /// test [`mul_add_assign`] with uneven vectors
 #[test]
 fn test_mul_add_assign_uneven() {
-    let mut a = vec![
-        TestScalar::from(1u32),
-        TestScalar::from(2u32),
-        TestScalar::from(3u32),
-        TestScalar::from(4u32),
-        TestScalar::from(5u32),
-    ];
-    let b = vec![
-        TestScalar::from(2u32),
-        TestScalar::from(3u32),
-        TestScalar::from(4u32),
-        TestScalar::from(5u32),
-    ];
+    let mut a = [1, 2, 3, 4, 5].map(TestScalar::from).to_vec();
+    let b = [2, 3, 4, 5].map(TestScalar::from).to_vec();
     mul_add_assign(&mut a, TestScalar::from(10u32), &b);
-    let c = vec![
-        TestScalar::from(1 + 10 * 2),
-        TestScalar::from(2 + 10 * 3),
-        TestScalar::from(3 + 10 * 4),
-        TestScalar::from(4 + 10 * 5),
-        TestScalar::from(5u32),
-    ];
+    let c = [1 + 10 * 2, 2 + 10 * 3, 3 + 10 * 4, 4 + 10 * 5, 5]
+        .map(TestScalar::from)
+        .to_vec();
     assert_eq!(a, c);
 }
 
@@ -53,12 +30,7 @@ fn test_mul_add_assign_uneven() {
     expected = "The length of result must be greater than or equal to the length of the vector of values to be multiplied and added"
 )]
 fn test_mul_add_assign_uneven_panic() {
-    let mut a = vec![
-        TestScalar::from(1u32),
-        TestScalar::from(2u32),
-        TestScalar::from(3u32),
-        TestScalar::from(4u32),
-    ];
+    let mut a = [1u32, 2u32, 3u32, 4u32].map(TestScalar::from).to_vec();
     let b = vec![2, 3, 4, 5, 6];
     mul_add_assign(&mut a, TestScalar::from(10u32), &b);
 }
@@ -66,30 +38,19 @@ fn test_mul_add_assign_uneven_panic() {
 /// test [`mul_add_assign`] with `TestScalar`
 #[test]
 fn test_mul_add_assign_testscalar() {
-    let mut a = vec![TestScalar::from(1u64), TestScalar::from(2u64)];
-    let b = vec![TestScalar::from(2u64), TestScalar::from(3u64)];
+    let mut a = [1, 2].map(TestScalar::from).to_vec();
+    let b = [2, 3].map(TestScalar::from).to_vec();
     mul_add_assign(&mut a, TestScalar::from(10u64), &b);
-    let c = vec![
-        TestScalar::from(1u64) + TestScalar::from(10u64) * TestScalar::from(2u64),
-        TestScalar::from(2u64) + TestScalar::from(10u64) * TestScalar::from(3u64),
-    ];
+    let c = [1 + 10 * 2, 2 + 10 * 3].map(TestScalar::from).to_vec();
     assert_eq!(a, c);
 }
 
 /// test [`mul_add_assign`] with uneven `TestScalar`
 #[test]
 fn test_mul_add_assign_testscalar_uneven() {
-    let mut a = vec![
-        TestScalar::from(1u64),
-        TestScalar::from(2u64),
-        TestScalar::from(3u64),
-    ];
-    let b = vec![TestScalar::from(2u64), TestScalar::from(3u64)];
+    let mut a = [1, 2, 3].map(TestScalar::from).to_vec();
+    let b = [2, 3].map(TestScalar::from).to_vec();
     mul_add_assign(&mut a, TestScalar::from(10u64), &b);
-    let c = vec![
-        TestScalar::from(1u64) + TestScalar::from(10u64) * TestScalar::from(2u64),
-        TestScalar::from(2u64) + TestScalar::from(10u64) * TestScalar::from(3u64),
-        TestScalar::from(3u64),
-    ];
+    let c = [1 + 10 * 2, 2 + 10 * 3, 3].map(TestScalar::from).to_vec();
     assert_eq!(a, c);
 }
