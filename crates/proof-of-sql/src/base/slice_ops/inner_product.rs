74e824fb6017ce476258e8858072954d844e4f32
Jacob Trombetta <jacob.trombetta@spaceandtime.io>
2025-05-20 13:36:27 -0400

    perf: do not create intermediate vector when calling the slice_ops module's inner_product

--- crates/proof-of-sql/src/base/slice_ops/inner_product.rs
+++ crates/proof-of-sql/src/base/slice_ops/inner_product.rs
@@ -9,13 +9,15 @@ use rayon::iter::{IndexedParallelIterator, IntoParallelRefIterator, ParallelIter
 
 /// This operation takes the inner product of two slices. In other words, it does `a[0] * b[0] + a[1] * b[1] + ... + a[n] * b[n]`.
 /// If one of the slices is longer than the other, the extra elements are ignored/considered to be 0.
-pub fn inner_product<F>(a: &[F], b: &[F]) -> F
+pub fn inner_product<'a, F, T>(a: &[F], b: &'a [T]) -> F
 where
     F: Sync + Send + Mul<Output = F> + Sum + Copy,
+    &'a T: Into<F>,
+    T: Sync,
 {
     if_rayon!(a.par_iter().with_min_len(super::MIN_RAYON_LEN), a.iter())
         .zip(b)
-        .map(|(&a, &b)| a * b)
+        .map(|(&a, b)| a * b.into())
         .sum()
 }
 
