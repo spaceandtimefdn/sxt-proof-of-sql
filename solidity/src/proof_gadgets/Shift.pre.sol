ff60e137e5384fa8a26932d1ee3b2feb3e4c1c2f
stuarttimwhite <stuart.white@spaceandtime.io>
2025-05-30 10:40:47 -0400

    feat(sol): shift expr (#863)
    Please be sure to look over the pull request guidelines here:
    https://github.com/spaceandtimelabs/sxt-proof-of-sql/blob/main/CONTRIBUTING.md#submit-pr.
    
    # Please go through the following checklist
    - [ ] The PR title and commit messages adhere to guidelines here:
    https://github.com/spaceandtimelabs/sxt-proof-of-sql/blob/main/CONTRIBUTING.md.
    In particular `!` is used if and only if at least one breaking change
    has been introduced.
    - [ ] I have run the ci check script with `source
    scripts/run_ci_checks.sh`.
    - [ ] I have run the clean commit check script with `source
    scripts/check_commits.sh`, and the commit history is certified to follow
    clean commit guidelines as described here:
    https://github.com/spaceandtimelabs/sxt-proof-of-sql/blob/main/COMMIT_GUIDELINES.md
    - [ ] The latest changes from `main` have been incorporated to this PR
    by simple rebase if possible, if not, then conflicts are resolved
    appropriately.
    
    # Rationale for this change
    
    <!--
    Why are you proposing this change? If this is already explained clearly
    in the linked issue then this section is not needed.
    Explaining clearly why changes are proposed helps reviewers understand
    your changes and offer better suggestions for fixes.
    
     Example:
     Add `NestedLoopJoinExec`.
     Closes #345.
    
    Since we added `HashJoinExec` in #323 it has been possible to do
    provable inner joins. However performance is not satisfactory in some
    cases. Hence we need to fix the problem by implement
    `NestedLoopJoinExec` and speed up the code
     for `HashJoinExec`.
    -->
    
    # What changes are included in this PR?
    
    <!--
    There is no need to duplicate the description in the ticket here but it
    is sometimes worth providing a summary of the individual changes in this
    PR.
    
    Example:
    - Add `NestedLoopJoinExec`.
    - Speed up `HashJoinExec`.
    - Route joins to `NestedLoopJoinExec` if the outer input is sufficiently
    small.
    -->
    
    # Are these changes tested?
    <!--
    We typically require tests for all PRs in order to:
    1. Prevent the code from being accidentally broken by subsequent changes
    2. Serve as another way to document the expected behavior of the code
    
    If tests are not included in your PR, please explain why (for example,
    are they covered by existing tests)?
    
    Example:
    Yes.
    -->
    
    ---------
    
    Co-authored-by: Ian Joiner <14581281+iajoiner@users.noreply.github.com>

---
+++ solidity/src/proof_gadgets/Shift.pre.sol
@@ -0,0 +1,123 @@
+// SPDX-License-Identifier: UNLICENSED
+// This is licensed under the Cryptographic Open Software License 1.0
+pragma solidity ^0.8.28;
+
+import "../base/Constants.sol";
+import "../base/Errors.sol";
+import {VerificationBuilder} from "../builder/VerificationBuilder.pre.sol";
+
+/// @title Shift
+/// @dev Library for handling column shifts
+library Shift {
+    /// @notice verifies that a shifted column is evaluated correctly
+    /// @custom:as-yul-wrapper
+    /// #### Wrapped Yul Function
+    /// ##### Signature
+    /// ```yul
+    /// shift_evaluate(builder_ptr, alpha, beta, expr_eval, shifted_expr_eval, chi_eval, chi_plus_one_eval)
+    /// ```
+    /// ##### Parameters
+    /// * `builder_ptr` - memory pointer to the verification builder
+    /// * `alpha` - a challenge
+    /// * `beta` - a challenge
+    /// * `expr_eval` - the expression evaluation
+    /// * `shifted_expr_eval` - the evaluation of the shifted expression
+    /// * `chi_eval` - the chi evaluation with length same as the column
+    /// * `chi_plus_one_eval` - the chi evaluation with length one longer than the column
+    /// @notice verifies that a shifted column is evaluated correctly
+    /// @param __builder The verification builder
+    /// @param __alpha a challenge
+    /// @param __beta a challenge
+    /// @param __exprEval the expression evaluation
+    /// @param __shiftedExprEval the evaluation of the shifted expression
+    /// @param __chiEval The chi value for evaluation
+    /// @param __chiPlusOneEval The chi plus one value for evaluation
+    /// @return __builderOut The verification builder result
+    function __shiftEvaluate( // solhint-disable-line gas-calldata-parameters
+        VerificationBuilder.Builder memory __builder,
+        uint256 __alpha,
+        uint256 __beta,
+        uint256 __exprEval,
+        uint256 __shiftedExprEval,
+        uint256 __chiEval,
+        uint256 __chiPlusOneEval
+    ) internal pure returns (VerificationBuilder.Builder memory __builderOut) {
+        assembly {
+            // IMPORT-YUL ../base/Errors.sol
+            function err(code) {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../base/Queue.pre.sol
+            function dequeue(queue_ptr) -> value {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../base/Array.pre.sol
+            function get_array_element(arr_ptr, index) -> value {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../builder/VerificationBuilder.pre.sol
+            function builder_consume_final_round_mle(builder_ptr) -> value {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../builder/VerificationBuilder.pre.sol
+            function builder_produce_identity_constraint(builder_ptr, evaluation, degree) {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../builder/VerificationBuilder.pre.sol
+            function builder_produce_zerosum_constraint(builder_ptr, evaluation, degree) {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../builder/VerificationBuilder.pre.sol
+            function builder_consume_rho_evaluation(builder_ptr) -> value {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../base/MathUtil.sol
+            function mulmod_bn254(lhs, rhs) -> product {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../base/MathUtil.sol
+            function addmod_bn254(lhs, rhs) -> difference {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../base/MathUtil.sol
+            function submod_bn254(lhs, rhs) -> product {
+                revert(0, 0)
+            }
+
+            function compute_shift_identity_constraint(star, chi_plus_one, fold) -> constraint {
+                constraint := addmod_bn254(submod_bn254(star, chi_plus_one), mulmod_bn254(fold, star))
+            }
+
+            function compute_shift_fold(alpha, beta, eval, rho) -> fold {
+                fold := mulmod_bn254(alpha, addmod_bn254(mulmod_bn254(beta, rho), eval))
+            }
+
+            function shift_evaluate(builder_ptr, alpha, beta, expr_eval, shifted_expr_eval, chi_eval, chi_plus_one_eval)
+            {
+                let rho_eval := builder_consume_rho_evaluation(builder_ptr)
+                let rho_plus_one_eval := builder_consume_rho_evaluation(builder_ptr)
+                let c_star_eval := builder_consume_final_round_mle(builder_ptr)
+                let d_star_eval := builder_consume_final_round_mle(builder_ptr)
+                // sum c_star - d_star = 0
+                builder_produce_zerosum_constraint(builder_ptr, submod_bn254(c_star_eval, d_star_eval), 1)
+                // c_star + c_fold * c_star - chi_n_plus_1 = 0
+                {
+                    let c_fold := compute_shift_fold(alpha, beta, expr_eval, addmod_bn254(rho_eval, chi_eval))
+                    builder_produce_identity_constraint(
+                        builder_ptr, compute_shift_identity_constraint(c_star_eval, chi_plus_one_eval, c_fold), 2
+                    )
+                }
+                // d_star + d_fold * d_star - chi_n_plus_1 = 0
+                {
+                    let d_fold := compute_shift_fold(alpha, beta, shifted_expr_eval, rho_plus_one_eval)
+                    builder_produce_identity_constraint(
+                        builder_ptr, compute_shift_identity_constraint(d_star_eval, chi_plus_one_eval, d_fold), 2
+                    )
+                }
+            }
+
+            shift_evaluate(__builder, __alpha, __beta, __exprEval, __shiftedExprEval, __chiEval, __chiPlusOneEval)
+        }
+        __builderOut = __builder;
+    }
+}
