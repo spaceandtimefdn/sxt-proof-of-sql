b4883c8d001d76675958ceca2d2be59e22116d4d
stuarttimwhite <stuart.white@spaceandtime.io>
2025-06-11 11:44:08 -0400

    feat(sol): inequality expr (#868)
    Please be sure to look over the pull request guidelines here:
    https://github.com/spaceandtimelabs/sxt-proof-of-sql/blob/main/CONTRIBUTING.md#submit-pr.
    
    # Please go through the following checklist
    - [ ] The PR title and commit messages adhere to guidelines here:
    https://github.com/spaceandtimelabs/sxt-proof-of-sql/blob/main/CONTRIBUTING.md.
    In particular `!` is used if and only if at least one breaking change
    has been introduced.
    - [ ] I have run the ci check script with `source
    scripts/run_ci_checks.sh`.
    - [ ] I have run the clean commit check script with `source
    scripts/check_commits.sh`, and the commit history is certified to follow
    clean commit guidelines as described here:
    https://github.com/spaceandtimelabs/sxt-proof-of-sql/blob/main/COMMIT_GUIDELINES.md
    - [ ] The latest changes from `main` have been incorporated to this PR
    by simple rebase if possible, if not, then conflicts are resolved
    appropriately.
    
    # Rationale for this change
    
    We need to add inequality to the solidity code still
    
    # What changes are included in this PR?
    
    New InequalityExpr code for solidity
    
    # Are these changes tested?
    Yes

--- solidity/src/proof_exprs/InequalityExpr.pre.sol
+++ solidity/src/proof_exprs/InequalityExpr.pre.sol
@@ -6,34 +6,36 @@ import "../base/Constants.sol";
 import "../base/Errors.sol";
 import {VerificationBuilder} from "../builder/VerificationBuilder.pre.sol";
 
-/// @title CastExpr
-/// @dev Library for handling casting a proof expression
-library CastExpr {
-    /// @notice Evaluates an cast expression by casting a sub-expression
+/// @title InequalityExpr
+/// @dev Library for handling inequality comparison expressions between two proof expressions
+library InequalityExpr {
+    /// @notice Evaluates an inequality expression by comparing two sub-expressions
     /// @custom:as-yul-wrapper
     /// #### Wrapped Yul Function
     /// ##### Signature
     /// ```yul
-    /// cast_expr_evaluate(expr_ptr, builder_ptr, chi_eval) -> expr_ptr_out, eval
+    /// inequality_expr_evaluate(expr_ptr, builder_ptr, chi_eval) -> expr_ptr_out, eval
     /// ```
     /// ##### Parameters
     /// * `expr_ptr` - calldata pointer to the expression data
     /// * `builder_ptr` - memory pointer to the verification builder
     /// * `chi_eval` - the chi value for evaluation
     /// ##### Return Values
-    /// * `expr_ptr_out` - pointer to the remaining expression after consuming the sub-expression
+    /// * `expr_ptr_out` - pointer to the remaining expression after consuming both sub-expressions
     /// * `eval` - the evaluation result from the builder's final round MLE
-    /// @notice Evaluates a sub-expression and casts it
+    /// @notice Evaluates two sub-expressions and uses sign gadget to check inequality
     /// ##### Proof Plan Encoding
-    /// The cast expression is encoded as follows:
-    /// 1. The input expression
-    /// @param __expr The cast expression data
+    /// The inequality expression is encoded as follows:
+    /// 1. A bit representing if it is a less than comparison
+    /// 2. The left hand side expression
+    /// 3. The right hand side expression
+    /// @param __expr The inqeuality expression data
     /// @param __builder The verification builder
     /// @param __chiEval The chi value for evaluation
     /// @return __exprOut The remaining expression after processing
     /// @return __builderOut The verification builder result
     /// @return __eval The evaluated result
-    function __castExprEvaluate( // solhint-disable-line gas-calldata-parameters
+    function __inequalityExprEvaluate( // solhint-disable-line gas-calldata-parameters
     bytes calldata __expr, VerificationBuilder.Builder memory __builder, uint256 __chiEval)
         external
         pure
@@ -76,28 +78,12 @@ library CastExpr {
             function get_array_element(arr_ptr, index) -> value {
                 revert(0, 0)
             }
-            // IMPORT-YUL ../base/DataType.pre.sol
-            function read_entry(result_ptr, data_type_variant) -> result_ptr_out, entry {
-                revert(0, 0)
-            }
-            // IMPORT-YUL ../base/DataType.pre.sol
-            function read_binary(result_ptr) -> result_ptr_out, entry {
-                revert(0, 0)
-            }
-            // IMPORT-YUL ../base/DataType.pre.sol
-            function read_data_type(ptr) -> ptr_out, data_type {
-                revert(0, 0)
-            }
             // IMPORT-YUL ColumnExpr.pre.sol
             function column_expr_evaluate(expr_ptr, builder_ptr) -> expr_ptr_out, eval {
                 revert(0, 0)
             }
             // IMPORT-YUL LiteralExpr.pre.sol
             function literal_expr_evaluate(expr_ptr, chi_eval) -> expr_ptr_out, eval {
-                revert(0, 0)
-            }
-            // IMPORT-YUL EqualsExpr.pre.sol
-            function equals_expr_evaluate(expr_ptr, builder_ptr, chi_eval) -> expr_ptr_out, eval {
                 revert(0, 0)
             }
             // IMPORT-YUL AddExpr.pre.sol
@@ -124,16 +110,12 @@ library CastExpr {
             function not_expr_evaluate(expr_ptr, builder_ptr, chi_eval) -> expr_ptr_out, eval {
                 revert(0, 0)
             }
-            // IMPORT-YUL ../builder/VerificationBuilder.pre.sol
-            function builder_consume_bit_distribution(builder_ptr) -> vary_mask, leading_bit_mask {
+            // IMPORT-YUL CastExpr.pre.sol
+            function cast_expr_evaluate(expr_ptr, builder_ptr, chi_eval) -> expr_ptr_out, eval {
                 revert(0, 0)
             }
-            // IMPORT-YUL ../proof_gadgets/SignExpr.pre.sol
-            function sign_expr_evaluate(expr_eval, builder_ptr, chi_eval) -> result_eval {
-                revert(0, 0)
-            }
-            // IMPORT-YUL ../proof_exprs/InequalityExpr.pre.sol
-            function inequality_expr_evaluate(expr_ptr, builder_ptr, chi_eval) -> expr_ptr_out, result_eval {
+            // IMPORT-YUL EqualsExpr.pre.sol
+            function equals_expr_evaluate(expr_ptr, builder_ptr, chi_eval) -> expr_ptr_out, eval {
                 revert(0, 0)
             }
             // IMPORT-YUL ../builder/VerificationBuilder.pre.sol
@@ -150,15 +132,47 @@ library CastExpr {
                 revert(0, 0)
             }
             // slither-disable-end cyclomatic-complexity
+            // IMPORT-YUL ../base/DataType.pre.sol
+            function read_entry(result_ptr, data_type_variant) -> result_ptr_out, entry {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../base/DataType.pre.sol
+            function read_binary(result_ptr) -> result_ptr_out, entry {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../builder/VerificationBuilder.pre.sol
+            function builder_consume_bit_distribution(builder_ptr) -> vary_mask, leading_bit_mask {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../proof_gadgets/SignExpr.pre.sol
+            function sign_expr_evaluate(expr_eval, builder_ptr, chi_eval) -> result_eval {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../base/DataType.pre.sol
+            function read_data_type(ptr) -> ptr_out, data_type {
+                revert(0, 0)
+            }
 
-            function cast_expr_evaluate(expr_ptr, builder_ptr, chi_eval) -> expr_ptr_out, result_eval {
-                let data_type
-                expr_ptr, data_type := read_data_type(expr_ptr)
-                expr_ptr_out, result_eval := proof_expr_evaluate(expr_ptr, builder_ptr, chi_eval)
+            function inequality_expr_evaluate(expr_ptr, builder_ptr, chi_eval) -> expr_ptr_out, result_eval {
+                let lhs_eval
+                expr_ptr, lhs_eval := proof_expr_evaluate(expr_ptr, builder_ptr, chi_eval)
+
+                let rhs_eval
+                expr_ptr, rhs_eval := proof_expr_evaluate(expr_ptr, builder_ptr, chi_eval)
+
+                let is_lt := shr(BOOLEAN_PADDING_BITS, calldataload(expr_ptr))
+                expr_ptr := add(expr_ptr, BOOLEAN_SIZE)
+
+                let diff_eval
+                switch is_lt
+                case 0 { diff_eval := submod_bn254(rhs_eval, lhs_eval) }
+                default { diff_eval := submod_bn254(lhs_eval, rhs_eval) }
+                result_eval := sign_expr_evaluate(diff_eval, builder_ptr, chi_eval)
+                expr_ptr_out := expr_ptr
             }
 
             let __exprOutOffset
-            __exprOutOffset, __eval := cast_expr_evaluate(__expr.offset, __builder, __chiEval)
+            __exprOutOffset, __eval := inequality_expr_evaluate(__expr.offset, __builder, __chiEval)
             __exprOut.offset := __exprOutOffset
             // slither-disable-next-line write-after-write
             __exprOut.length := sub(__expr.length, sub(__exprOutOffset, __expr.offset))
