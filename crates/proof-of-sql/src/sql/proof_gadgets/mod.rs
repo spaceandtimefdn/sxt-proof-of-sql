b98b9f8d08d93929f9c824f6db1bd05883ac4fe3
stuarttimwhite <stuart.white@spaceandtime.io>
2025-06-30 23:43:37 -0400

    feat: fold log expr gadget

--- crates/proof-of-sql/src/sql/proof_gadgets/mod.rs
+++ crates/proof-of-sql/src/sql/proof_gadgets/mod.rs
@@ -1,6 +1,7 @@
 //! This module contains shared proof logic for multiple `ProofExpr` / `ProofPlan` implementations.
 #[cfg(test)]
 mod divide_and_modulo_expr;
+pub(crate) mod fold_log_expr;
 mod membership_check;
 mod monotonic;
 #[cfg_attr(not(test), expect(dead_code))]
