8736073d197f71568c0b4c66953bf0af787a2761
stuarttimwhite <stuart.white@spaceandtime.io>
2025-07-21 12:03:17 -0400

    feat(sol): `fold_log_star_evaluate_from_column_exprs`

--- solidity/test/proof_gadgets/FoldLog.t.pre.sol
+++ solidity/test/proof_gadgets/FoldLog.t.pre.sol
@@ -42,7 +42,7 @@ contract FoldLogExprTest is Test {
             builder.constraintMultipliers[i] = 1;
         }
 
-        for (uint8 i = 0; i < 1; ++i) {
+        for (uint8 i = 0; i < 8; ++i) {
             uint256[] memory columnEval = new uint256[](2);
             columnEval[0] = column[i][0];
             columnEval[1] = column[i][1];
@@ -52,6 +52,57 @@ contract FoldLogExprTest is Test {
                 __alpha: alpha,
                 __beta: beta,
                 __columnEvals: columnEval,
+                __chiEval: 1
+            });
+        }
+        assert(builder.aggregateEvaluation == 0);
+    }
+
+    function testSimpleFoldLogExprEvalsWithMultipleColumnExprs() public pure {
+        bytes memory plan = abi.encodePacked(uint64(0), uint64(1), hex"abcdef");
+        VerificationBuilder.Builder memory builder;
+        builder.maxDegree = 3;
+        builder.rowMultipliersEvaluation = 1;
+        uint256[2][8] memory column = [
+            [uint256(300), 2],
+            [uint256(1), 1],
+            [uint256(MODULUS_MINUS_ONE), 3],
+            [uint256(1), 1],
+            [uint256(MODULUS_MINUS_ONE), 3],
+            [uint256(1), 1],
+            [uint256(5), 0],
+            [uint256(2), 1]
+        ];
+        builder.finalRoundMLEs = new uint256[](8);
+        {
+            uint256 inv7207 = 5767416846624501685451078744310193616366497016288337618798791418108430738612;
+            uint256 inv28 = 11725844395628183154774860220673540226008052357365732684124037957094183122652;
+            uint256 invNegative14 = 20324796952422184134943091049167469725080624086100603319148332458963250745930;
+            uint256 inv121 = 18632140626441697090011403237698341604301500274734310226453843233200894835112;
+            uint256 inv52 = 21467315124303904544895513327079250567614742008100341375550161798372427563009;
+            uint256[8] memory star = [inv7207, inv28, invNegative14, inv28, invNegative14, inv28, inv121, inv52];
+            for (uint8 i = 0; i < 8; ++i) {
+                builder.finalRoundMLEs[i] = star[i];
+            }
+        }
+
+        builder.constraintMultipliers = new uint256[](24);
+        for (uint8 i = 0; i < 8; ++i) {
+            builder.constraintMultipliers[i] = 1;
+        }
+
+        for (uint8 i = 0; i < 8; ++i) {
+            uint256[] memory columnEval = new uint256[](2);
+            columnEval[0] = column[i][0];
+            columnEval[1] = column[i][1];
+            builder.columnEvaluations = columnEval;
+            uint256 actualStar;
+            (builder, actualStar) = FoldLogExpr.__foldLogStarEvaluateFromColumnExprs({
+                __plan: plan,
+                __builder: builder,
+                __alpha: 3,
+                __beta: 8,
+                __columnCount: 2,
                 __chiEval: 1
             });
         }
