4534db8b9fc368f362a675b3781b50fcc1a27e83
stuarttimwhite <stuart.white@spaceandtime.io>
2025-07-22 13:27:38 -0400

    temp

--- solidity/test/proof_plans/SortMergeJoinExec.t.pre.sol
+++ solidity/test/proof_plans/SortMergeJoinExec.t.pre.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.28;
 
 import {Test} from "forge-std/Test.sol";
 import "../../src/base/Constants.sol";
+import {Errors} from "../../src/base/Errors.sol";
 import {VerificationBuilder} from "../../src/builder/VerificationBuilder.pre.sol";
 import {SortMergeJoinExec} from "../../src/proof_plans/SortMergeJoinExec.pre.sol";
 
@@ -291,5 +292,258 @@ contract SortMergeJoinExecTest is Test {
             (planOut, builder, evals, chiLength, chiEval) = SortMergeJoinExec.__sortMergeJoinEvaluate(plan, builder);
         }
         assert(builder.aggregateEvaluation == 0);
+    }
+
+    function testSortMergeJoinExecTooManyJoinColumns() public {
+        VerificationBuilder.Builder memory builder;
+        bytes memory plan;
+
+        {
+            bytes memory leftPlan = abi.encodePacked(
+                TABLE_EXEC_VARIANT,
+                uint64(0), // table_ref
+                uint64(3), // column_count
+                uint64(0), // column1_index
+                uint64(1), // column2_index
+                uint64(2) // column3_index
+            );
+
+            bytes memory leftJoinIndices = abi.encodePacked(
+                uint64(2), // join index count
+                uint64(1),
+                uint64(2)
+            );
+
+            bytes memory rightPlan = abi.encodePacked(
+                TABLE_EXEC_VARIANT,
+                uint64(1), // table_ref
+                uint64(3), // column_count
+                uint64(3), // column1_index
+                uint64(4), // column2_index
+                uint64(5) // column3_index
+            );
+
+            bytes memory rightJoinIndices = abi.encodePacked(
+                uint64(1), // join index count
+                uint64(0)
+            );
+            plan = abi.encodePacked(leftPlan, leftJoinIndices, rightPlan, rightJoinIndices, hex"abcdef");
+        }
+
+        // challenges
+        {
+            builder.challenges = new uint256[](2 * ROW_TOTAL);
+            for (uint8 i = 0; i < ROW_TOTAL; ++i) {
+                builder.challenges[2 * i] = 3; // alpha
+                builder.challenges[2 * i + 1] = 8; // beta
+            }
+        }
+
+        // chi evals
+        builder.chiEvaluations = new uint256[](8 * CHI_TOTAL);
+        {
+            uint256[CHI_TOTAL] memory lengths = [uint256(3), 2, 3, 4];
+            for (uint8 i = 0; i < ROW_TOTAL; ++i) {
+                for (uint8 j = 0; j < CHI_TOTAL; ++j) {
+                    uint256 length = lengths[j];
+                    builder.chiEvaluations[2 * (i * CHI_TOTAL + j)] = length;
+                    uint256 eval = 0;
+                    if (length > i) {
+                        eval = 1;
+                    }
+                    builder.chiEvaluations[2 * (i * CHI_TOTAL + j) + 1] = eval;
+                }
+            }
+        }
+
+        builder.tableChiEvaluations = new uint256[](4);
+        builder.tableChiEvaluations[0] = 3;
+        builder.tableChiEvaluations[1] = 1;
+        builder.tableChiEvaluations[2] = 2;
+        builder.tableChiEvaluations[3] = 1;
+        builder.singletonChiEvaluation = 1;
+
+        // rho evals
+        builder.rhoEvaluations = new uint256[](ROW_TOTAL * RHO_TOTAL);
+        {
+            uint256[RHO_TOTAL] memory lengths = [uint256(2), 3, 3, 2, 3, 4];
+            for (uint8 i = 0; i < ROW_TOTAL; ++i) {
+                for (uint8 j = 0; j < RHO_TOTAL; ++j) {
+                    uint256 eval = 0;
+                    if (lengths[j] > i) {
+                        eval = i;
+                    }
+                    builder.rhoEvaluations[i * RHO_TOTAL + j] = eval;
+                }
+            }
+        }
+
+        // mles
+        builder.finalRoundMLEs = new uint256[](ROW_TOTAL * (FINAL_ROUND_MLE_TOTAL + VARYING_BIT_TOTAL));
+        {
+            uint256[FINAL_ROUND_MLE_TOTAL][ROW_TOTAL] memory finalRoundMLEs = [
+                [
+                    MODULUS_MINUS_ONE, // u eval
+                    0, // shifted u eval
+                    14923801958072233106077094826311778469464793909374568870703321036301687610648, // c_star for shift gadget
+                    1, // d_star for shift gadget
+                    1, // bit eval for sign gadget
+                    MODULUS_MINUS_ONE, // join eval
+                    1, // left output column eval 0
+                    3, // left output column eval 2
+                    0, // left rho eval
+                    1, // left hat multiplicity
+                    10074446955173859956738117514536196637923519413252069308468488945040793052664,
+                    10074446955173859956738117514536196637923519413252069308468488945040793052664,
+                    2, // left join multiplicity
+                    10944121435919637611123202872628637544274182200208017171849102093287904247808,
+                    10944121435919637611123202872628637544274182200208017171849102093287904247808,
+                    5, // right output column eval 1
+                    6, // right output column eval 2
+                    1, // right rho eval
+                    1, // right hat multiplicity
+                    8039225893859938267920868448655644390720148984628380656337469920855181461851,
+                    460266789361106254673405728288120270553587101877907264236644480558837094534,
+                    1, // right join multiplicity
+                    10944121435919637611123202872628637544274182200208017171849102093287904247808,
+                    16416182153879456416684804308942956316411273300312025757773653139931856371713,
+                    0, // shifted i eval
+                    11725844395628183154774860220673540226008052357365732684124037957094183122652,
+                    1
+                ],
+                [
+                    uint256(1), // u eval
+                    MODULUS_MINUS_ONE, // shifted u eval
+                    21467315124303904544895513327079250567614742008100341375550161798372427563009, // c_star for shift gadget
+                    14923801958072233106077094826311778469464793909374568870703321036301687610648, // d_star for shift gadget
+                    0, // bit eval for sign gadget
+                    1, // join eval
+                    1, // left column eval 0
+                    3, // left column eval 2
+                    1, // left rho eval
+                    1, // left hat multiplicity
+                    2851295496054451040591965271693713772621322413579694052532748328073899665449,
+                    2851295496054451040591965271693713772621322413579694052532748328073899665449,
+                    1, // left join multiplicity
+                    16416182153879456416684804308942956316411273300312025757773653139931856371713,
+                    16416182153879456416684804308942956316411273300312025757773653139931856371713,
+                    5, // right output column eval 1
+                    6, // right output column eval 2
+                    0, // right rho eval
+                    2, // right hat multiplicity
+                    460266789361106254673405728288120270553587101877907264236644480558837094534,
+                    8039225893859938267920868448655644390720148984628380656337469920855181461851,
+                    1, // right join multiplicity
+                    16416182153879456416684804308942956316411273300312025757773653139931856371713,
+                    10944121435919637611123202872628637544274182200208017171849102093287904247808,
+                    1, // shifted i eval
+                    6496471390482283403974624657428010136580938079855111550940942529249946921050,
+                    11725844395628183154774860220673540226008052357365732684124037957094183122652
+                ],
+                [
+                    uint256(0), // u eval
+                    1, // shifted u eval
+                    1, // c_star for shift gadget
+                    21467315124303904544895513327079250567614742008100341375550161798372427563009, // d_star for shift gadget
+                    1, // bit eval for sign gadget
+                    MODULUS_MINUS_ONE, // join eval
+                    1, // left column eval 0
+                    3, // left column eval 2
+                    2, // left rho eval
+                    1, // left hat multiplicity
+                    19344707929420007666775874800946745888535234308035673040517464253432216520237,
+                    19344707929420007666775874800946745888535234308035673040517464253432216520237,
+                    0, // left join multiplicity
+                    0,
+                    10944121435919637611123202872628637544274182200208017171849102093287904247808,
+                    5, // right output column eval 1
+                    6, // right output column eval 2
+                    1, // right rho eval
+                    0, // right hat multiplicity
+                    0,
+                    460266789361106254673405728288120270553587101877907264236644480558837094534,
+                    0, // right join multiplicity
+                    0,
+                    0,
+                    18446744073709551616, // shifted i eval
+                    18735783757041287116993268168181089483405786268842422841364722931849478463737,
+                    6496471390482283403974624657428010136580938079855111550940942529249946921050
+                ],
+                [
+                    uint256(0), // u eval
+                    0, // shifted u eval
+                    0, // c_star for shift gadget
+                    0, // d_star for shift gadget
+                    0, // bit eval for sign gadget
+                    0, // join eval
+                    0, // left column eval 0
+                    0, // left column eval 2
+                    0, // left rho eval
+                    0, // left hat multiplicity
+                    0,
+                    0,
+                    0, // left join multiplicity
+                    0,
+                    0,
+                    0, // right output column eval 1
+                    0, // right output column eval 2
+                    0, // right rho eval
+                    0, // right hat multiplicity
+                    0,
+                    0,
+                    0, // right join multiplicity
+                    0,
+                    0,
+                    36893488147419103233, // shifted i eval
+                    1,
+                    18735783757041287116993268168181089483405786268842422841364722931849478463737
+                ]
+            ];
+            uint256[5][ROW_TOTAL] memory bitEvals =
+                [[uint256(1), 1, 1, 1, 0], [uint256(1), 0, 1, 1, 0], [uint256(1), 1, 0, 1, 0], [uint256(1), 0, 0, 1, 1]];
+
+            for (uint8 i = 0; i < ROW_TOTAL; ++i) {
+                for (uint8 j = 0; j < FINAL_ROUND_MLE_TOTAL; ++j) {
+                    builder.finalRoundMLEs[i * (FINAL_ROUND_MLE_TOTAL + VARYING_BIT_TOTAL) + j] = finalRoundMLEs[i][j];
+                }
+                builder.finalRoundMLEs[i * (FINAL_ROUND_MLE_TOTAL + VARYING_BIT_TOTAL) + FINAL_ROUND_MLE_TOTAL] =
+                    bitEvals[i][0];
+                for (uint8 j = 1; j < VARYING_BIT_TOTAL - 3; ++j) {
+                    builder.finalRoundMLEs[i * (FINAL_ROUND_MLE_TOTAL + VARYING_BIT_TOTAL) + FINAL_ROUND_MLE_TOTAL + j]
+                    = bitEvals[i][1];
+                }
+                builder.finalRoundMLEs[(i + 1) * (FINAL_ROUND_MLE_TOTAL + VARYING_BIT_TOTAL) - 3] = bitEvals[i][2];
+                builder.finalRoundMLEs[(i + 1) * (FINAL_ROUND_MLE_TOTAL + VARYING_BIT_TOTAL) - 2] = bitEvals[i][3];
+                builder.finalRoundMLEs[(i + 1) * (FINAL_ROUND_MLE_TOTAL + VARYING_BIT_TOTAL) - 1] = bitEvals[i][4];
+            }
+        }
+
+        // bit distributions
+        {
+            uint256[2 * BIT_DITRIBUTION_TOTAL] memory bitDistributions = [
+                0x8000000000000000000000000000000000000000000000000000000000000000,
+                1,
+                57896044618658097711785492504343953926634992332820282019802578980251403026431,
+                0
+            ];
+            builder.bitDistributions = new uint256[](2 * BIT_DITRIBUTION_TOTAL * ROW_TOTAL);
+            for (uint8 i = 0; i < ROW_TOTAL; ++i) {
+                for (uint8 j = 0; j < 2 * BIT_DITRIBUTION_TOTAL; ++j) {
+                    builder.bitDistributions[2 * i * BIT_DITRIBUTION_TOTAL + j] = bitDistributions[j];
+                }
+            }
+        }
+
+        builder.maxDegree = 3;
+        builder.rowMultipliersEvaluation = 1;
+        builder.aggregateEvaluation = 0;
+        builder.constraintMultipliers = new uint256[](1000);
+        for (uint16 i = 0; i < 1000; ++i) {
+            builder.constraintMultipliers[i] = 1;
+        }
+        builder.columnEvaluations = new uint256[](6);
+
+        vm.expectRevert(Errors.NumberOfJoinColumnsNotOne.selector);
+        SortMergeJoinExec.__sortMergeJoinEvaluate(plan, builder);
     }
 }
