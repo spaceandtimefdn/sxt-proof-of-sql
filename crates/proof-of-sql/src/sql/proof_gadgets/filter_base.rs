ec2f691698d2a5475e8d4e0bb7819f5f21565cc9
stuarttimwhite <stuart.white@spaceandtime.io>
2025-08-05 23:23:04 -0400

    refactor: use fold gadget in filter gadget

--- crates/proof-of-sql/src/sql/proof_gadgets/filter_base.rs
+++ crates/proof-of-sql/src/sql/proof_gadgets/filter_base.rs
@@ -1,44 +1,31 @@
 use crate::{
-    base::{database::Column, proof::ProofError, scalar::Scalar, slice_ops},
+    base::{database::Column, proof::ProofError, scalar::Scalar},
     sql::{
         proof::{FinalRoundBuilder, SumcheckSubpolynomialType, VerificationBuilder},
-        proof_plans::{fold_columns, fold_vals},
+        proof_gadgets::fold_log_expr::FoldLogExpr,
     },
 };
-use alloc::{boxed::Box, vec};
-use ark_ff::{One, Zero};
+use alloc::{boxed::Box, vec, vec::Vec};
 use bumpalo::Bump;
 
-#[expect(clippy::similar_names, clippy::too_many_arguments)]
+#[expect(clippy::similar_names)]
 pub(crate) fn verify_evaluate_filter<S: Scalar>(
     builder: &mut impl VerificationBuilder<S>,
     alpha: S,
     beta: S,
     columns_evals: &[S],
-    filtered_column_evals: &[S],
     chi_n_eval: S,
     chi_m_eval: S,
     s_eval: S,
-) -> Result<(), ProofError> {
-    let c_fold_eval = alpha * fold_vals(beta, columns_evals);
-    let d_fold_eval = alpha * fold_vals(beta, filtered_column_evals);
-
-    let c_star_eval = builder.try_consume_final_round_mle_evaluation()?;
-    let d_star_eval = builder.try_consume_final_round_mle_evaluation()?;
-
-    // c_star + c_fold * c_star - chi_n = 0
-    builder.try_produce_sumcheck_subpolynomial_evaluation(
-        SumcheckSubpolynomialType::Identity,
-        c_star_eval + c_fold_eval * c_star_eval - chi_n_eval,
-        2,
-    )?;
-
-    // d_star + d_fold * d_star - chi_m = 0
-    builder.try_produce_sumcheck_subpolynomial_evaluation(
-        SumcheckSubpolynomialType::Identity,
-        d_star_eval + d_fold_eval * d_star_eval - chi_m_eval,
-        2,
-    )?;
+) -> Result<Vec<S>, ProofError> {
+    let fold_gadget = FoldLogExpr::new(alpha, beta);
+    let c_star_eval = fold_gadget
+        .verify_evaluate(builder, columns_evals, chi_n_eval)?
+        .0;
+    let filtered_columns_evals =
+        builder.try_consume_first_round_mle_evaluations(columns_evals.len())?;
+    let (d_star_eval, d_fold_eval) =
+        fold_gadget.verify_evaluate(builder, &filtered_columns_evals, chi_m_eval)?;
 
     // sum c_star * s - d_star = 0
     builder.try_produce_sumcheck_subpolynomial_evaluation(
@@ -54,7 +41,7 @@ pub(crate) fn verify_evaluate_filter<S: Scalar>(
         2,
     )?;
 
-    Ok(())
+    Ok(filtered_columns_evals)
 }
 
 /// Below are the mappings between the names of the parameters in the math and the code
@@ -75,73 +62,24 @@ pub(crate) fn final_round_evaluate_filter<'a, S: Scalar + 'a>(
     input_length: usize,
     output_length: usize,
 ) {
-    let chi_n = alloc.alloc_slice_fill_copy(input_length, true);
+    let fold_gadget = FoldLogExpr::new(alpha, beta);
+    let c_star = fold_gadget
+        .final_round_evaluate(builder, alloc, columns, input_length)
+        .0;
     let chi_m = alloc.alloc_slice_fill_copy(output_length, true);
-
-    let c_fold = alloc.alloc_slice_fill_copy(input_length, Zero::zero());
-    fold_columns(c_fold, alpha, beta, columns);
-    let d_fold = alloc.alloc_slice_fill_copy(output_length, Zero::zero());
-    fold_columns(d_fold, alpha, beta, filtered_columns);
-
-    let c_star = alloc.alloc_slice_copy(c_fold);
-    slice_ops::add_const::<S, S>(c_star, One::one());
-    slice_ops::batch_inversion(c_star);
-
-    let d_star = alloc.alloc_slice_copy(d_fold);
-    slice_ops::add_const::<S, S>(d_star, One::one());
-    slice_ops::batch_inversion(d_star);
-
-    builder.produce_intermediate_mle(c_star as &[_]);
-    builder.produce_intermediate_mle(d_star as &[_]);
-
-    final_round_filter_constraints(
+    let (d_star, d_fold) = fold_gadget.final_round_evaluate_with_chi(
         builder,
-        c_star as &[_],
-        d_star as &[_],
-        s,
-        c_fold as &[_],
-        d_fold as &[_],
-        chi_n,
+        alloc,
+        filtered_columns,
+        output_length,
         chi_m,
-    );
-}
-
-#[expect(clippy::too_many_arguments)]
-pub(crate) fn final_round_filter_constraints<'a, S: Scalar + 'a>(
-    builder: &mut FinalRoundBuilder<'a, S>,
-    c_star: &'a [S],
-    d_star: &'a [S],
-    selection: &'a [bool],
-    c_fold: &'a [S],
-    d_fold: &'a [S],
-    input_chi: &'a [bool],
-    output_chi: &'a [bool],
-) {
-    // c_star + c_fold * c_star - chi_n = 0
-    builder.produce_sumcheck_subpolynomial(
-        SumcheckSubpolynomialType::Identity,
-        vec![
-            (S::one(), vec![Box::new(c_star)]),
-            (S::one(), vec![Box::new(c_star), Box::new(c_fold)]),
-            (-S::one(), vec![Box::new(input_chi)]),
-        ],
-    );
-
-    // d_star + d_fold * d_star - chi_m = 0
-    builder.produce_sumcheck_subpolynomial(
-        SumcheckSubpolynomialType::Identity,
-        vec![
-            (S::one(), vec![Box::new(d_star)]),
-            (S::one(), vec![Box::new(d_star), Box::new(d_fold)]),
-            (-S::one(), vec![Box::new(output_chi)]),
-        ],
     );
 
     // sum c_star * s - d_star = 0
     builder.produce_sumcheck_subpolynomial(
         SumcheckSubpolynomialType::ZeroSum,
         vec![
-            (S::one(), vec![Box::new(c_star), Box::new(selection)]),
+            (S::one(), vec![Box::new(c_star), Box::new(s)]),
             (-S::one(), vec![Box::new(d_star)]),
         ],
     );
@@ -150,7 +88,7 @@ pub(crate) fn final_round_filter_constraints<'a, S: Scalar + 'a>(
     builder.produce_sumcheck_subpolynomial(
         SumcheckSubpolynomialType::Identity,
         vec![
-            (S::one(), vec![Box::new(d_fold), Box::new(output_chi)]),
+            (S::one(), vec![Box::new(d_fold), Box::new(chi_m as &[_])]),
             (-S::one(), vec![Box::new(d_fold)]),
         ],
     );
