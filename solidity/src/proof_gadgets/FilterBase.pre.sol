b88983e81efccf8d39091d7b832b9bb27da8fd12
stuarttimwhite <stuart.white@spaceandtime.io>
2025-08-05 23:23:52 -0400

    feat(sol): add filter gadget helper functions

--- solidity/src/proof_gadgets/FilterBase.pre.sol
+++ solidity/src/proof_gadgets/FilterBase.pre.sol
@@ -93,4 +93,360 @@ library FilterBase {
         }
         __builderOut = __builder;
     }
+
+    /// @notice Verifies the filtered columns
+    /// @custom:as-yul-wrapper
+    /// #### Wrapped Yul Function
+    /// ##### Signature
+    /// ```yul
+    /// function verify_filter_from_column_evals(builder_ptr, alpha, beta, column_evals, input_chi_eval, output_chi_eval, selection_eval) -> filtered_columns
+    /// ```
+    /// ##### Parameters
+    /// * `builder_ptr` - memory pointer to the verification builder
+    /// * `column_evals` - the input columns
+    /// * `input_chi_eval` - the column of ones with same length as the input columns
+    /// * `output_chi_eval` - column of ones with same length as the filtered columns
+    /// * `selection_eval` - column of ones and zeroes which indicate which rows to include in the result
+    /// ##### Return Values
+    /// * `filtered_columns` - The output columns
+    /// @param __builder The verification builder
+    /// @param __columnEvals The input columns
+    /// @param __inputChiEval The column of ones with same length as the input columns
+    /// @param __outputChiEval The column of ones with same length as the filtered columns
+    /// @param __selectionEval The column of ones and zeroes which indicate which rows to include in the result
+    /// @return __builderOut The updated verification builder
+    /// @return __filteredColumns The output column
+    function __filterBaseEvaluateFromColumnEvals( // solhint-disable-line gas-calldata-parameters
+        VerificationBuilder.Builder memory __builder,
+        uint256[] memory __columnEvals,
+        uint256 __inputChiEval,
+        uint256 __outputChiEval,
+        uint256 __selectionEval
+    ) external pure returns (VerificationBuilder.Builder memory __builderOut, uint256[] memory __filteredColumns) {
+        assembly {
+            // IMPORT-YUL ../base/Errors.sol
+            function err(code) {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../base/MathUtil.pre.sol
+            function addmod_bn254(lhs, rhs) -> sum {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../base/MathUtil.pre.sol
+            function submod_bn254(lhs, rhs) -> difference {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../base/MathUtil.pre.sol
+            function mulmod_bn254(lhs, rhs) -> product {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../base/Queue.pre.sol
+            function dequeue(queue_ptr) -> value {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../builder/VerificationBuilder.pre.sol
+            function builder_consume_final_round_mle(builder_ptr) -> value {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../builder/VerificationBuilder.pre.sol
+            function builder_produce_identity_constraint(builder_ptr, evaluation, degree) {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../builder/VerificationBuilder.pre.sol
+            function builder_produce_zerosum_constraint(builder_ptr, evaluation, degree) {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../builder/VerificationBuilder.pre.sol
+            function builder_consume_challenge(builder_ptr) -> value {
+                revert(0, 0)
+            }
+            // IMPORT-YUL FoldLogExpr.pre.sol
+            function fold_log_star_evaluate_from_fold(builder_ptr, fold, chi_eval) -> star {
+                revert(0, 0)
+            }
+            // IMPORT-YUL FoldLogExpr.pre.sol
+            function fold_log_star_evaluate(builder_ptr, alpha, beta, column_evals, chi_eval) -> star {
+                revert(0, 0)
+            }
+            // IMPORT-YUL FoldUtil.pre.sol
+            function fold_first_round_mles(builder_ptr, beta, column_count) -> fold, evaluations_ptr {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../builder/VerificationBuilder.pre.sol
+            function builder_consume_first_round_mle(builder_ptr) -> value {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../base/MathUtil.pre.sol
+            function compute_fold(beta, evals) -> fold {
+                revert(0, 0)
+            }
+            // IMPORT-YUL FoldLogExpr.pre.sol
+            function fold_log_evaluate_from_mles(builder_ptr, alpha, beta, column_count, chi_eval) ->
+                fold,
+                star,
+                evaluations_ptr
+            {
+                revert(0, 0)
+            }
+            function verify_filter_from_column_evals(
+                builder_ptr, column_evals, input_chi_eval, output_chi_eval, selection_eval
+            ) -> filtered_columns {
+                let alpha := builder_consume_challenge(builder_ptr)
+                let beta := builder_consume_challenge(builder_ptr)
+                let c_star := fold_log_star_evaluate(builder_ptr, alpha, beta, column_evals, input_chi_eval)
+                let d_fold, d_star
+                d_fold, d_star, filtered_columns :=
+                    fold_log_evaluate_from_mles(builder_ptr, alpha, beta, mload(column_evals), output_chi_eval)
+
+                builder_produce_zerosum_constraint(
+                    builder_ptr, submod_bn254(mulmod_bn254(c_star, selection_eval), d_star), 2
+                )
+                builder_produce_identity_constraint(
+                    builder_ptr, mulmod_bn254(d_fold, submod_bn254(output_chi_eval, 1)), 2
+                )
+            }
+
+            __filteredColumns :=
+                verify_filter_from_column_evals(__builder, __columnEvals, __inputChiEval, __outputChiEval, __selectionEval)
+        }
+        __builderOut = __builder;
+    }
+
+    /// @notice Verifies the filtered columns
+    /// @custom:as-yul-wrapper
+    /// #### Wrapped Yul Function
+    /// ##### Signature
+    /// ```yul
+    /// function verify_filter_from_expr_evals(plan_ptr, builder_ptr, num_columns, input_chi_eval, output_chi_eval, selection_eval) -> plan_ptr_out, filtered_columns
+    /// ```
+    /// ##### Parameters
+    /// * `plan_ptr` - calldata pointer to the plan
+    /// * `builder_ptr` - memory pointer to the verification builder
+    /// * `num_columns` - the number of columns
+    /// * `input_chi_eval` - the column of ones with same length as the input columns
+    /// * `output_chi_eval` - column of ones with same length as the filtered columns
+    /// * `selection_eval` - column of ones and zeroes which indicate which rows to include in the result
+    /// ##### Return Values
+    /// * `plan_ptr_out` - pointer to the remaining plan after consuming
+    /// * `filtered_columns` - consumed star value
+    /// @param __plan The plan data
+    /// @param __builder The verification builder
+    /// @param __numColumns The number of columns
+    /// @param __inputChiEval The column of ones with same length as the input columns
+    /// @param __outputChiEval The column of ones with same length as the filtered columns
+    /// @param __selectionEval The column of ones and zeroes which indicate which rows to include in the result
+    /// @return __builderOut The updated verification builder
+    /// @return __filteredColumns The output column
+    function __filterBaseEvaluateFromExprEvals( // solhint-disable-line gas-calldata-parameters
+        bytes calldata __plan,
+        VerificationBuilder.Builder memory __builder,
+        uint256 __numColumns,
+        uint256 __inputChiEval,
+        uint256 __outputChiEval,
+        uint256 __selectionEval
+    ) external pure returns (VerificationBuilder.Builder memory __builderOut, uint256[] memory __filteredColumns) {
+        assembly {
+            // IMPORT-YUL ../base/Errors.sol
+            function err(code) {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../base/MathUtil.pre.sol
+            function addmod_bn254(lhs, rhs) -> sum {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../base/MathUtil.pre.sol
+            function submod_bn254(lhs, rhs) -> difference {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../base/MathUtil.pre.sol
+            function mulmod_bn254(lhs, rhs) -> product {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../base/Queue.pre.sol
+            function dequeue(queue_ptr) -> value {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../builder/VerificationBuilder.pre.sol
+            function builder_consume_final_round_mle(builder_ptr) -> value {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../builder/VerificationBuilder.pre.sol
+            function builder_produce_identity_constraint(builder_ptr, evaluation, degree) {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../builder/VerificationBuilder.pre.sol
+            function builder_produce_zerosum_constraint(builder_ptr, evaluation, degree) {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../builder/VerificationBuilder.pre.sol
+            function builder_consume_challenge(builder_ptr) -> value {
+                revert(0, 0)
+            }
+            // IMPORT-YUL FoldLogExpr.pre.sol
+            function fold_log_star_evaluate_from_fold(builder_ptr, fold, chi_eval) -> star {
+                revert(0, 0)
+            }
+            // IMPORT-YUL FoldLogExpr.pre.sol
+            function fold_log_star_evaluate_from_expr_evals(
+                plan_ptr, builder_ptr, input_chi_eval, alpha, beta, column_count
+            ) -> plan_ptr_out, star {
+                revert(0, 0)
+            }
+            // IMPORT-YUL FoldUtil.pre.sol
+            function fold_first_round_mles(builder_ptr, beta, column_count) -> fold, evaluations_ptr {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../builder/VerificationBuilder.pre.sol
+            function builder_consume_first_round_mle(builder_ptr) -> value {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../base/MathUtil.pre.sol
+            function compute_fold(beta, evals) -> fold {
+                revert(0, 0)
+            }
+            // IMPORT-YUL FoldLogExpr.pre.sol
+            function fold_log_evaluate_from_mles(builder_ptr, alpha, beta, column_count, chi_eval) ->
+                fold,
+                star,
+                evaluations_ptr
+            {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../base/Queue.pre.sol
+            function dequeue_uint512(queue_ptr) -> value {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../base/SwitchUtil.pre.sol
+            function case_const(lhs, rhs) {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../builder/VerificationBuilder.pre.sol
+            function builder_get_column_evaluation(builder_ptr, column_num) -> eval {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../base/Array.pre.sol
+            function get_array_element(arr_ptr, index) -> value {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../proof_exprs/ColumnExpr.pre.sol
+            function column_expr_evaluate(expr_ptr, builder_ptr) -> expr_ptr_out, eval {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../proof_exprs/LiteralExpr.pre.sol
+            function literal_expr_evaluate(expr_ptr, chi_eval) -> expr_ptr_out, eval {
+                revert(0, 0)
+            }
+            // slither-disable-start cyclomatic-complexity
+            // IMPORT-YUL ../base/DataType.pre.sol
+            function read_entry(result_ptr, data_type_variant) -> result_ptr_out, entry {
+                revert(0, 0)
+            }
+            // slither-disable-end cyclomatic-complexity
+            // IMPORT-YUL ../base/DataType.pre.sol
+            function read_binary(result_ptr) -> result_ptr_out, entry {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../base/DataType.pre.sol
+            function read_data_type(ptr) -> ptr_out, data_type {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../proof_exprs/EqualsExpr.pre.sol
+            function equals_expr_evaluate(expr_ptr, builder_ptr, chi_eval) -> expr_ptr_out, result_eval {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../proof_exprs/AddExpr.pre.sol
+            function add_expr_evaluate(expr_ptr, builder_ptr, chi_eval) -> expr_ptr_out, result_eval {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../proof_exprs/SubtractExpr.pre.sol
+            function subtract_expr_evaluate(expr_ptr, builder_ptr, chi_eval) -> expr_ptr_out, result_eval {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../proof_exprs/MultiplyExpr.pre.sol
+            function multiply_expr_evaluate(expr_ptr, builder_ptr, chi_eval) -> expr_ptr_out, result_eval {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../proof_exprs/AndExpr.pre.sol
+            function and_expr_evaluate(expr_ptr, builder_ptr, chi_eval) -> expr_ptr_out, result_eval {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../proof_exprs/OrExpr.pre.sol
+            function or_expr_evaluate(expr_ptr, builder_ptr, chi_eval) -> expr_ptr_out, result_eval {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../proof_exprs/NotExpr.pre.sol
+            function not_expr_evaluate(expr_ptr, builder_ptr, chi_eval) -> expr_ptr_out, result_eval {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../proof_exprs/CastExpr.pre.sol
+            function cast_expr_evaluate(expr_ptr, builder_ptr, chi_eval) -> expr_ptr_out, result_eval {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../builder/VerificationBuilder.pre.sol
+            function builder_consume_bit_distribution(builder_ptr) -> vary_mask, leading_bit_mask {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../builder/VerificationBuilder.pre.sol
+            function builder_get_placeholder_parameter(builder_ptr, index) -> value {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../proof_gadgets/SignExpr.pre.sol
+            function sign_expr_evaluate(expr_eval, builder_ptr, chi_eval) -> result_eval {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../proof_exprs/InequalityExpr.pre.sol
+            function inequality_expr_evaluate(expr_ptr, builder_ptr, chi_eval) -> expr_ptr_out, result_eval {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../proof_exprs/PlaceholderExpr.pre.sol
+            function placeholder_expr_evaluate(expr_ptr, builder_ptr, chi_eval) -> expr_ptr_out, result_eval {
+                revert(0, 0)
+            }
+            // IMPORT-YUL ../proof_exprs/ScalingCastExpr.pre.sol
+            function scaling_cast_expr_evaluate(expr_ptr, builder_ptr, chi_eval) -> expr_ptr_out, result_eval {
+                revert(0, 0)
+            }
+            // slither-disable-start cyclomatic-complexity
+            // IMPORT-YUL ../proof_exprs/ProofExpr.pre.sol
+            function proof_expr_evaluate(expr_ptr, builder_ptr, chi_eval) -> expr_ptr_out, eval {
+                revert(0, 0)
+            }
+            // slither-disable-end cyclomatic-complexity
+            // IMPORT-YUL FoldUtil.pre.sol
+            function fold_expr_evals(plan_ptr, builder_ptr, input_chi_eval, beta, column_count) -> plan_ptr_out, fold {
+                revert(0, 0)
+            }
+            function verify_filter_from_expr_evals(
+                plan_ptr, builder_ptr, num_columns, input_chi_eval, output_chi_eval, selection_eval
+            ) -> plan_ptr_out, filtered_columns {
+                let c_star, d_fold, d_star
+                {
+                    let alpha := builder_consume_challenge(builder_ptr)
+                    let beta := builder_consume_challenge(builder_ptr)
+
+                    plan_ptr, c_star :=
+                        fold_log_star_evaluate_from_expr_evals(
+                            plan_ptr, builder_ptr, input_chi_eval, alpha, beta, num_columns
+                        )
+                    d_fold, d_star, filtered_columns :=
+                        fold_log_evaluate_from_mles(builder_ptr, alpha, beta, num_columns, output_chi_eval)
+                }
+
+                builder_produce_zerosum_constraint(
+                    builder_ptr, submod_bn254(mulmod_bn254(c_star, selection_eval), d_star), 2
+                )
+                builder_produce_identity_constraint(
+                    builder_ptr, mulmod_bn254(d_fold, submod_bn254(output_chi_eval, 1)), 2
+                )
+                plan_ptr_out := plan_ptr
+            }
+            // We don't keep the plan out, because it isn't needed for testing.
+            let __planOutOffset
+            __planOutOffset, __filteredColumns :=
+                verify_filter_from_expr_evals(
+                    __plan.offset, __builder, __numColumns, __inputChiEval, __outputChiEval, __selectionEval
+                )
+        }
+        __builderOut = __builder;
+    }
 }
--- solidity/test/proof_gadgets/FilterBase.t.pre.sol
+++ solidity/test/proof_gadgets/FilterBase.t.pre.sol
@@ -56,4 +56,146 @@ contract FilterBaseTest is Test {
 
         assert(builder.aggregateEvaluation == 0);
     }
+
+    function testFilterBaseEvaluateFromColumnEvals() public pure {
+        VerificationBuilder.Builder memory builder;
+
+        // max degree and row multipliers
+        builder.maxDegree = 3;
+        builder.rowMultipliersEvaluation = 1;
+
+        // constraint multipliers
+        builder.constraintMultipliers = new uint256[](16); // 4 constraints times 4 rows
+        for (uint8 i = 0; i < 16; ++i) {
+            builder.constraintMultipliers[i] = 1;
+        }
+
+        // challenges
+        builder.challenges = new uint256[](8);
+        builder.challenges[0] = 3; // alpha
+        builder.challenges[1] = 8; // beta
+        builder.challenges[2] = 3; // alpha
+        builder.challenges[3] = 8; // beta
+        builder.challenges[4] = 3; // alpha
+        builder.challenges[5] = 8; // beta
+        builder.challenges[6] = 3; // alpha
+        builder.challenges[7] = 8; // beta
+
+        uint256[4] memory columnEval = [uint256(1), 3, MODULUS_MINUS_ONE, 400];
+
+        // first round mles
+        builder.firstRoundMLEs = new uint256[](4); // 1 mle times 4 rows
+        {
+            uint256[4] memory filteredColumn = [columnEval[0], columnEval[2], 0, 0];
+            for (uint8 i = 0; i < 4; ++i) {
+                builder.firstRoundMLEs[i] = filteredColumn[i];
+            }
+        }
+
+        // final round mles
+        builder.finalRoundMLEs = new uint256[](8); // 2 mles times 4 rows
+        {
+            uint256 inv4 = 16416182153879456416684804308942956316411273300312025757773653139931856371713;
+            uint256 inv10 = 15321770010287492655572484021680092561983855080291224040588742930603065946932;
+            uint256 invNegative2 = 10944121435919637611123202872628637544274182200208017171849102093287904247808;
+            uint256 inv1201 = 17860514583182755801666509267570465934036134148549303627663813574391583951461;
+            uint256[4] memory cStarColumn = [inv4, inv10, invNegative2, inv1201];
+            uint256[4] memory dStarColumn = [cStarColumn[0], cStarColumn[2], 0, 0];
+
+            for (uint8 i = 0; i < 4; ++i) {
+                builder.finalRoundMLEs[i * 2] = cStarColumn[i];
+                builder.finalRoundMLEs[i * 2 + 1] = dStarColumn[i];
+            }
+        }
+        builder.aggregateEvaluation = 0;
+        uint256[4] memory inputChiEval = [uint256(1), 1, 1, 1];
+        uint256[4] memory outputChiEval = [uint256(1), 1, 0, 0];
+        uint256[4] memory selectionEval = [uint256(1), 0, 1, 0];
+        for (uint8 i = 0; i < 4; ++i) {
+            uint256[] memory columnEvals = new uint256[](1);
+            columnEvals[0] = columnEval[i];
+            uint256[] memory filteredColumnEvals;
+            (builder, filteredColumnEvals) = FilterBase.__filterBaseEvaluateFromColumnEvals({
+                __builder: builder,
+                __columnEvals: columnEvals,
+                __inputChiEval: inputChiEval[i],
+                __outputChiEval: outputChiEval[i],
+                __selectionEval: selectionEval[i]
+            });
+        }
+
+        assert(builder.aggregateEvaluation == 0);
+    }
+
+    function testFilterBaseEvaluateFromExprEvals() public pure {
+        bytes memory plan = abi.encodePacked(COLUMN_EXPR_VARIANT, uint64(0));
+        VerificationBuilder.Builder memory builder;
+
+        // max degree and row multipliers
+        builder.maxDegree = 3;
+        builder.rowMultipliersEvaluation = 1;
+
+        // constraint multipliers
+        builder.constraintMultipliers = new uint256[](16); // 4 constraints times 4 rows
+        for (uint8 i = 0; i < 16; ++i) {
+            builder.constraintMultipliers[i] = 1;
+        }
+
+        // challenges
+        builder.challenges = new uint256[](8);
+        builder.challenges[0] = 3; // alpha
+        builder.challenges[1] = 8; // beta
+        builder.challenges[2] = 3; // alpha
+        builder.challenges[3] = 8; // beta
+        builder.challenges[4] = 3; // alpha
+        builder.challenges[5] = 8; // beta
+        builder.challenges[6] = 3; // alpha
+        builder.challenges[7] = 8; // beta
+
+        uint256[4] memory columnEval = [uint256(1), 3, MODULUS_MINUS_ONE, 400];
+
+        // first round mles
+        builder.firstRoundMLEs = new uint256[](4); // 1 mle times 4 rows
+        {
+            uint256[4] memory filteredColumn = [columnEval[0], columnEval[2], 0, 0];
+            for (uint8 i = 0; i < 4; ++i) {
+                builder.firstRoundMLEs[i] = filteredColumn[i];
+            }
+        }
+
+        // final round mles
+        builder.finalRoundMLEs = new uint256[](8); // 2 mles times 4 rows
+        {
+            uint256 inv4 = 16416182153879456416684804308942956316411273300312025757773653139931856371713;
+            uint256 inv10 = 15321770010287492655572484021680092561983855080291224040588742930603065946932;
+            uint256 invNegative2 = 10944121435919637611123202872628637544274182200208017171849102093287904247808;
+            uint256 inv1201 = 17860514583182755801666509267570465934036134148549303627663813574391583951461;
+            uint256[4] memory cStarColumn = [inv4, inv10, invNegative2, inv1201];
+            uint256[4] memory dStarColumn = [cStarColumn[0], cStarColumn[2], 0, 0];
+
+            for (uint8 i = 0; i < 4; ++i) {
+                builder.finalRoundMLEs[i * 2] = cStarColumn[i];
+                builder.finalRoundMLEs[i * 2 + 1] = dStarColumn[i];
+            }
+        }
+        builder.aggregateEvaluation = 0;
+        uint256[4] memory inputChiEval = [uint256(1), 1, 1, 1];
+        uint256[4] memory outputChiEval = [uint256(1), 1, 0, 0];
+        uint256[4] memory selectionEval = [uint256(1), 0, 1, 0];
+        builder.columnEvaluations = new uint256[](1);
+        for (uint8 i = 0; i < 4; ++i) {
+            builder.columnEvaluations[0] = columnEval[i];
+            uint256[] memory filteredColumnEvals;
+            (builder, filteredColumnEvals) = FilterBase.__filterBaseEvaluateFromExprEvals({
+                __plan: plan,
+                __builder: builder,
+                __numColumns: 1,
+                __inputChiEval: inputChiEval[i],
+                __outputChiEval: outputChiEval[i],
+                __selectionEval: selectionEval[i]
+            });
+        }
+
+        assert(builder.aggregateEvaluation == 0);
+    }
 }
