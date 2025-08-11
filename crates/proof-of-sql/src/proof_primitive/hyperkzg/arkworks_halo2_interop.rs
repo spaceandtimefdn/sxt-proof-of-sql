64cf3cd64e3ba62b4104878811062c775378db57
Jacob Trombetta <jacob.trombetta@spaceandtime.io>
2025-08-11 15:42:03 -0400

    refactor: update the copyright to point to only point to the original project source

--- crates/proof-of-sql/src/proof_primitive/hyperkzg/arkworks_halo2_interop.rs
+++ crates/proof-of-sql/src/proof_primitive/hyperkzg/arkworks_halo2_interop.rs
@@ -1,20 +1,5 @@
-// Copyright 2025-present Space and Time Labs, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 // This code is adapted from the blitzar-rs project:
 // https://github.com/spaceandtimefdn/blitzar-rs/blob/main/src/compute/arkworks_halo2_interop.rs
-// Original copyright held by Space and Time Labs, Inc.
 
 use ark_bn254::{Fq as Bn254Fq, G1Affine as Bn254G1Affine};
 use ark_ff::BigInteger256;
