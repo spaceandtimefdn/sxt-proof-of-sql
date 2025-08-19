b533d31e6908fc96112b0040fc318e3b7b6fbc00
Ian Joiner <14581281+iajoiner@users.noreply.github.com>
2025-08-19 17:13:16 -0400

    refactor!: remove `sql/parse` from `proof-of-sql` crate

--- crates/proof-of-sql/src/sql/mod.rs
+++ crates/proof-of-sql/src/sql/mod.rs
@@ -3,7 +3,6 @@
 mod error;
 /// This module holds the [`EVMProofPlan`] struct and its implementation, which allows for EVM compatible serialization.
 pub mod evm_proof_plan;
-pub mod parse;
 /// [`AnalyzeError`] temporarily exists until we switch to using Datafusion Analyzer to handle type checking.
 pub use error::{AnalyzeError, AnalyzeResult};
 pub mod postprocessing;
