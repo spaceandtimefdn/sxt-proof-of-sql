db8d1d780f36fa3be34b3a59aa24b8b47e387933
Ian Joiner <14581281+iajoiner@users.noreply.github.com>
2025-11-03 10:56:39 -0500

    feat: make `BigDecimalExt` public

--- crates/proof-of-sql/src/base/math/mod.rs
+++ crates/proof-of-sql/src/base/math/mod.rs
@@ -8,6 +8,6 @@ pub mod i256;
 mod log;
 pub(crate) use log::log2_up;
 mod big_decimal_ext;
-pub(crate) use big_decimal_ext::BigDecimalExt;
+pub use big_decimal_ext::BigDecimalExt;
 /// TODO: add docs
 pub(crate) mod permutation;
