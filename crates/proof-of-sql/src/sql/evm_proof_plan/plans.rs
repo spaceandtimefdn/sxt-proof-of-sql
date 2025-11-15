62e0d15a9c9d06aeb38b1b7d01ec084d4bc43cf7
stuarttimwhite <stuart.white@spaceandtime.io>
2025-11-14 22:52:42 -0500

    fix: Use result fields `EVMFilterExec::try_from_proof_plan` for exprs

--- crates/proof-of-sql/src/sql/evm_proof_plan/plans.rs
+++ crates/proof-of-sql/src/sql/evm_proof_plan/plans.rs
@@ -262,17 +262,28 @@ impl EVMFilterExec {
         table_refs: &IndexSet<TableRef>,
         column_refs: &IndexSet<ColumnRef>,
     ) -> EVMProofPlanResult<Self> {
+        let input_result_column_refs: IndexSet<ColumnRef> = plan
+            .input()
+            .get_column_result_fields()
+            .into_iter()
+            .map(|f| ColumnRef::new(TableRef::from_names(None, ""), f.name(), f.data_type()))
+            .collect();
         Ok(Self {
             input_plan: Box::new(EVMDynProofPlan::try_from_proof_plan(
                 plan.input(),
                 table_refs,
                 column_refs,
             )?),
-            where_clause: EVMDynProofExpr::try_from_proof_expr(plan.where_clause(), column_refs)?,
+            where_clause: EVMDynProofExpr::try_from_proof_expr(
+                plan.where_clause(),
+                &input_result_column_refs,
+            )?,
             results: plan
                 .aliased_results()
                 .iter()
-                .map(|result| EVMDynProofExpr::try_from_proof_expr(&result.expr, column_refs))
+                .map(|result| {
+                    EVMDynProofExpr::try_from_proof_expr(&result.expr, &input_result_column_refs)
+                })
                 .collect::<Result<_, _>>()?,
         })
     }
