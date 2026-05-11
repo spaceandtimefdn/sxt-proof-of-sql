89bceb9f7d2592c0c227bbb5fd92dd31b8537365
Dustin Vali <dvfarhad@gmail.com>
2026-05-11 05:42:42 -0400

    test: cover borrowed table accessors

--- crates/proof-of-sql/src/base/database/table_test_accessor.rs
+++ crates/proof-of-sql/src/base/database/table_test_accessor.rs
@@ -168,3 +168,212 @@ impl<'a, CP: CommitmentEvaluationProof> TableTestAccessor<'a, CP> {
         res
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::base::{
+        commitment::{
+            naive_commitment::NaiveCommitment, naive_evaluation_proof::NaiveEvaluationProof,
+            Commitment, CommittableColumn,
+        },
+        database::table_utility::*,
+        map::indexset,
+        math::decimal::Precision,
+        posql_time::{PoSQLTimeUnit, PoSQLTimeZone},
+        scalar::{test_scalar::TestScalar, ScalarExt},
+    };
+    use bumpalo::Bump;
+
+    type Accessor<'a> = TableTestAccessor<'a, NaiveEvaluationProof>;
+
+    fn sample_table<'a>(alloc: &'a Bump) -> Table<'a, TestScalar> {
+        let raw_bytes = [
+            alloc.alloc_slice_copy(&[1_u8, 2][..]) as &[u8],
+            alloc.alloc_slice_copy(&[3_u8, 4, 5][..]) as &[u8],
+        ];
+        let byte_scalars = [
+            TestScalar::from_byte_slice_via_hash(raw_bytes[0]),
+            TestScalar::from_byte_slice_via_hash(raw_bytes[1]),
+        ];
+        table([
+            borrowed_boolean("bool", [true, false], alloc),
+            borrowed_uint8("u8", [1_u8, 2], alloc),
+            borrowed_tinyint("i8", [-1_i8, 2], alloc),
+            borrowed_smallint("i16", [-3_i16, 4], alloc),
+            borrowed_int("i32", [-5_i32, 6], alloc),
+            borrowed_bigint("i64", [-7_i64, 8], alloc),
+            borrowed_int128("i128", [-9_i128, 10], alloc),
+            borrowed_decimal75("decimal", 10, 2, [11, 12], alloc),
+            borrowed_scalar("scalar", [13, 14], alloc),
+            borrowed_varchar("text", ["a", "bc"], alloc),
+            (
+                "bytes".into(),
+                Column::VarBinary((
+                    alloc.alloc_slice_copy(&raw_bytes),
+                    alloc.alloc_slice_copy(&byte_scalars),
+                )),
+            ),
+            borrowed_timestamptz(
+                "time",
+                PoSQLTimeUnit::Second,
+                PoSQLTimeZone::utc(),
+                [100, 200],
+                alloc,
+            ),
+        ])
+    }
+
+    #[test]
+    fn table_test_accessor_exposes_metadata_and_schema() {
+        let alloc = Bump::new();
+        let table_ref = TableRef::new("sxt", "borrowed");
+        let accessor = Accessor::new_from_table(table_ref.clone(), sample_table(&alloc), 7, ());
+
+        assert_eq!(accessor.get_length(&table_ref), 2);
+        assert_eq!(accessor.get_offset(&table_ref), 7);
+        assert_eq!(
+            accessor.get_column_names(&table_ref),
+            vec![
+                "bool", "u8", "i8", "i16", "i32", "i64", "i128", "decimal", "scalar", "text",
+                "bytes", "time"
+            ]
+        );
+        assert_eq!(
+            accessor.lookup_column(&table_ref, &"decimal".into()),
+            Some(ColumnType::Decimal75(Precision::new(10).unwrap(), 2))
+        );
+        assert_eq!(accessor.lookup_column(&table_ref, &"missing".into()), None);
+        assert_eq!(
+            accessor.lookup_schema(&table_ref)[..3],
+            [
+                ("bool".into(), ColumnType::Boolean),
+                ("u8".into(), ColumnType::Uint8),
+                ("i8".into(), ColumnType::TinyInt),
+            ]
+        );
+    }
+
+    #[test]
+    fn table_test_accessor_returns_borrowed_columns_for_each_type() {
+        let alloc = Bump::new();
+        let table_ref = TableRef::new("sxt", "borrowed");
+        let accessor = Accessor::new_from_table(table_ref.clone(), sample_table(&alloc), 0, ());
+
+        match accessor.get_column(&table_ref, &"bool".into()) {
+            Column::Boolean(col) => assert_eq!(col, &[true, false]),
+            _ => panic!("expected boolean column"),
+        }
+        match accessor.get_column(&table_ref, &"u8".into()) {
+            Column::Uint8(col) => assert_eq!(col, &[1, 2]),
+            _ => panic!("expected uint8 column"),
+        }
+        match accessor.get_column(&table_ref, &"i8".into()) {
+            Column::TinyInt(col) => assert_eq!(col, &[-1, 2]),
+            _ => panic!("expected tinyint column"),
+        }
+        match accessor.get_column(&table_ref, &"i16".into()) {
+            Column::SmallInt(col) => assert_eq!(col, &[-3, 4]),
+            _ => panic!("expected smallint column"),
+        }
+        match accessor.get_column(&table_ref, &"i32".into()) {
+            Column::Int(col) => assert_eq!(col, &[-5, 6]),
+            _ => panic!("expected int column"),
+        }
+        match accessor.get_column(&table_ref, &"i64".into()) {
+            Column::BigInt(col) => assert_eq!(col, &[-7, 8]),
+            _ => panic!("expected bigint column"),
+        }
+        match accessor.get_column(&table_ref, &"i128".into()) {
+            Column::Int128(col) => assert_eq!(col, &[-9, 10]),
+            _ => panic!("expected int128 column"),
+        }
+        match accessor.get_column(&table_ref, &"decimal".into()) {
+            Column::Decimal75(precision, scale, col) => {
+                assert_eq!(precision.value(), 10);
+                assert_eq!(scale, 2);
+                assert_eq!(col, &[TestScalar::from(11), TestScalar::from(12)]);
+            }
+            _ => panic!("expected decimal column"),
+        }
+        match accessor.get_column(&table_ref, &"scalar".into()) {
+            Column::Scalar(col) => assert_eq!(col, &[TestScalar::from(13), TestScalar::from(14)]),
+            _ => panic!("expected scalar column"),
+        }
+        match accessor.get_column(&table_ref, &"text".into()) {
+            Column::VarChar((strings, scalars)) => {
+                assert_eq!(strings, &["a", "bc"]);
+                assert_eq!(scalars, &[TestScalar::from("a"), TestScalar::from("bc")]);
+            }
+            _ => panic!("expected varchar column"),
+        }
+        match accessor.get_column(&table_ref, &"bytes".into()) {
+            Column::VarBinary((bytes, scalars)) => {
+                assert_eq!(bytes[0], &[1_u8, 2]);
+                assert_eq!(bytes[1], &[3_u8, 4, 5]);
+                assert_eq!(scalars.len(), 2);
+            }
+            _ => panic!("expected varbinary column"),
+        }
+        match accessor.get_column(&table_ref, &"time".into()) {
+            Column::TimestampTZ(unit, zone, col) => {
+                assert_eq!(unit, PoSQLTimeUnit::Second);
+                assert_eq!(zone, PoSQLTimeZone::utc());
+                assert_eq!(col, &[100, 200]);
+            }
+            _ => panic!("expected timestamp column"),
+        }
+    }
+
+    #[test]
+    fn table_test_accessor_builds_tables_and_updates_offsets() {
+        let alloc = Bump::new();
+        let table_ref = TableRef::new("sxt", "borrowed");
+        let mut accessor = Accessor::new_from_table(table_ref.clone(), sample_table(&alloc), 0, ());
+
+        let selected = indexset! {"i64".into(), "text".into()};
+        let table = accessor.get_table(&table_ref, &selected);
+        assert_eq!(table.num_columns(), 2);
+        assert_eq!(table.num_rows(), 2);
+
+        let empty = accessor.get_table(&table_ref, &indexset! {});
+        assert_eq!(empty.num_columns(), 0);
+        assert_eq!(empty.num_rows(), 2);
+
+        let original_commitment = accessor.get_commitment(&table_ref, &"i64".into());
+        accessor.update_offset(&table_ref, 5);
+        assert_eq!(accessor.get_offset(&table_ref), 5);
+        assert_ne!(
+            original_commitment,
+            accessor.get_commitment(&table_ref, &"i64".into())
+        );
+
+        assert_eq!(
+            accessor.get_commitment(&table_ref, &"i64".into()),
+            NaiveCommitment::compute_commitments(
+                &[CommittableColumn::from(&[-7_i64, 8][..])],
+                5,
+                &()
+            )[0]
+        );
+    }
+
+    #[test]
+    fn cloned_table_test_accessor_keeps_tables_and_setup() {
+        let alloc = Bump::new();
+        let table_ref = TableRef::new("sxt", "borrowed");
+        let accessor = Accessor::new_from_table(table_ref.clone(), sample_table(&alloc), 3, ());
+        let clone = accessor.clone();
+
+        assert_eq!(clone.get_length(&table_ref), 2);
+        assert_eq!(clone.get_offset(&table_ref), 3);
+        assert_eq!(
+            clone.get_commitment(&table_ref, &"u8".into()),
+            NaiveCommitment::compute_commitments(
+                &[CommittableColumn::from(&[1_u8, 2][..])],
+                3,
+                &()
+            )[0]
+        );
+    }
+}
