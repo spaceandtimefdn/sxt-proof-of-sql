2c317b0f4ce93d69f8c99ca0dd3246f6945c028f
Valentin Cazin <valentin.cazin@gmail.com>
2026-05-27 11:11:32 +0200

    test: restore full dory module list

--- crates/proof-of-sql/src/proof_primitive/dory/mod.rs
+++ crates/proof-of-sql/src/proof_primitive/dory/mod.rs
@@ -88,3 +88,81 @@ pub(crate) use extended_dory_inner_product::{
 
 #[cfg(test)]
 mod extended_dory_inner_product_test;
+
+mod public_parameters;
+pub use public_parameters::PublicParameters;
+
+mod eval_vmv_re;
+pub(crate) use eval_vmv_re::{eval_vmv_re_prove, eval_vmv_re_verify};
+
+#[cfg(test)]
+mod eval_vmv_re_test;
+
+mod vmv_state;
+#[cfg(test)]
+use vmv_state::VMV;
+pub(crate) use vmv_state::{VMVProverState, VMVVerifierState};
+
+#[cfg(test)]
+mod vmv_state_test;
+
+mod dory_public_setup;
+pub use dory_public_setup::{DoryProverPublicSetup, DoryVerifierPublicSetup};
+
+mod dory_commitment;
+#[cfg(test)]
+mod dory_commitment_test;
+
+#[cfg(not(feature = "blitzar"))]
+mod dory_commitment_helper_cpu;
+#[cfg(not(feature = "blitzar"))]
+use dory_commitment_helper_cpu::compute_dory_commitments;
+#[cfg(feature = "blitzar")]
+mod dory_commitment_helper_gpu;
+pub use dory_commitment::{DoryCommitment, DoryScalar};
+#[cfg(feature = "blitzar")]
+use dory_commitment_helper_gpu::compute_dory_commitments;
+#[cfg(test)]
+mod dory_compute_commitments_test;
+
+mod dory_vmv_helper;
+use dory_vmv_helper::{
+    compute_L_R_vec, compute_T_vec_prime, compute_l_r_tensors, compute_nu, compute_v_vec,
+};
+mod build_vmv_state;
+use build_vmv_state::{build_vmv_prover_state, build_vmv_verifier_state};
+
+mod dory_commitment_evaluation_proof;
+pub use dory_commitment_evaluation_proof::DoryEvaluationProof;
+#[cfg(test)]
+mod dory_commitment_evaluation_proof_test;
+
+mod deferred_msm;
+type DeferredGT = deferred_msm::DeferredMSM<GT, F>;
+type DeferredG1 = deferred_msm::DeferredMSM<G1Affine, F>;
+type DeferredG2 = deferred_msm::DeferredMSM<G2Affine, F>;
+
+mod blitzar_metadata_table;
+mod offset_to_bytes;
+mod pack_scalars;
+mod pairings;
+mod transpose;
+
+mod dynamic_build_vmv_state;
+#[cfg(not(feature = "blitzar"))]
+mod dynamic_dory_commitment_helper_cpu;
+#[cfg(feature = "blitzar")]
+mod dynamic_dory_commitment_helper_gpu;
+mod dynamic_dory_helper;
+#[cfg(not(feature = "blitzar"))]
+use dynamic_dory_commitment_helper_cpu::compute_dynamic_dory_commitments;
+#[cfg(feature = "blitzar")]
+use dynamic_dory_commitment_helper_gpu::compute_dynamic_dory_commitments;
+mod dynamic_dory_commitment;
+mod dynamic_dory_commitment_evaluation_proof;
+#[cfg(test)]
+mod dynamic_dory_compute_commitments_test;
+pub use dynamic_dory_commitment::DynamicDoryCommitment;
+#[cfg(test)]
+mod dynamic_dory_commitment_evaluation_proof_test;
+pub use dynamic_dory_commitment_evaluation_proof::DynamicDoryEvaluationProof;
