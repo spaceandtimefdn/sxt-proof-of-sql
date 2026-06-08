79af06416b7750a6ebc2b270db6fc7a10b27340c
elianguitarra <290944581+elianguitarra@users.noreply.github.com>
2026-06-07 19:55:03 -0700

    test: cover verifier state constructor

--- crates/proof-of-sql/src/proof_primitive/dory/state_test.rs
+++ crates/proof-of-sql/src/proof_primitive/dory/state_test.rs
@@ -1,4 +1,4 @@
-use super::{rand_G_vecs, test_rng, ProverState, PublicParameters};
+use super::{rand_G_vecs, test_rng, ProverSetup, ProverState, PublicParameters, VerifierState};
 use ark_ec::pairing::Pairing;
 
 #[test]
@@ -21,4 +21,23 @@ pub fn we_can_create_a_verifier_state_from_a_prover_state() {
         assert_eq!(verifier_state.D_2, D_2);
         assert_eq!(verifier_state.nu, nu);
     }
+}
+
+#[test]
+pub fn we_can_create_a_verifier_state_from_commitments() {
+    let mut rng = test_rng();
+    let nu = 3;
+    let pp = PublicParameters::test_rand(nu, &mut rng);
+    let prover_setup: ProverSetup = (&pp).into();
+    let (v1, v2) = rand_G_vecs(nu, &mut rng);
+
+    let C = Pairing::multi_pairing(&v1, &v2);
+    let D_1 = Pairing::multi_pairing(&v1, prover_setup.Gamma_2[nu]);
+    let D_2 = Pairing::multi_pairing(prover_setup.Gamma_1[nu], &v2);
+    let verifier_state = VerifierState::new(C.into(), D_1.into(), D_2.into(), nu);
+
+    assert_eq!(verifier_state.C, C);
+    assert_eq!(verifier_state.D_1, D_1);
+    assert_eq!(verifier_state.D_2, D_2);
+    assert_eq!(verifier_state.nu, nu);
 }
