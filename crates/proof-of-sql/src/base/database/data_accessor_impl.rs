cc78ea4082018f17a555c369bb10dc90ec9650bb
stuarttimwhite <stuart.white@spaceandtime.io>
2026-06-22 12:23:12 -0400

    feat: add `DataAccessorImpl::try_from_record_batch`

--- crates/proof-of-sql/src/base/database/data_accessor_impl.rs
+++ crates/proof-of-sql/src/base/database/data_accessor_impl.rs
@@ -1,8 +1,13 @@
+#[cfg(feature = "arrow")]
+use crate::base::database::{ArrayRefExt, ArrowArrayToColumnConversionError};
 use crate::base::{
     database::{Column, DataAccessor, MetadataAccessor, TableRef},
     scalar::Scalar,
     IndexMap,
 };
+#[cfg(feature = "arrow")]
+use arrow::array::RecordBatch;
+use bumpalo::Bump;
 use sqlparser::ast::Ident;
 
 /// The canonical implementation for the `DataAccessor` trait
@@ -29,6 +34,31 @@ impl<'a, S: Scalar> TableDataAccessor<'a, S> {
     #[must_use]
     pub fn new(offset: usize, table_data: IndexMap<Ident, Column<'a, S>>) -> Self {
         Self { offset, table_data }
+    }
+
+    /// Creates a new instance of `TableDataAccessor` using a `RecordBatch`
+    #[cfg(feature = "arrow")]
+    pub fn try_from_record_batch(
+        record_batch: &'a RecordBatch,
+        offset: usize,
+        alloc: &'a Bump,
+    ) -> Result<Self, ArrowArrayToColumnConversionError> {
+        let range = 0..record_batch.num_rows();
+        let columns = record_batch
+            .schema()
+            .fields()
+            .iter()
+            .zip(record_batch.columns())
+            .map(|(f, col)| {
+                col.to_column::<S>(alloc, &range, None)
+                    .map(|col| (f.name().as_str().into(), col))
+            })
+            // Use collect to transform Iterator<Result<T, E>> into Result<Collection<T>, E>
+            .collect::<Result<IndexMap<_, _>, _>>()?;
+        Ok(Self {
+            offset,
+            table_data: columns,
+        })
     }
 }
 
@@ -69,7 +99,12 @@ mod tests {
         },
         scalar::test_scalar::TestScalar,
     };
+    use alloc::sync::Arc;
+    #[cfg(feature = "arrow")]
+    use arrow::array::{ArrayRef, BooleanArray, RecordBatch};
+    use bumpalo::Bump;
     use sqlparser::ast::Ident;
+    use std::str::FromStr;
 
     #[test]
     fn we_can_get_offset_and_length() {
@@ -86,5 +121,32 @@ mod tests {
         assert_eq!(data_accessor.get_length(&table_ref), 1);
         assert_eq!(data_accessor.get_offset(&table_ref), 2);
         assert_eq!(data_accessor.get_column(&table_ref, &column_id), column);
+    }
+
+    #[cfg(feature = "arrow")]
+    #[test]
+    fn we_can_get_data_accessor_from_record_batch() {
+        let rb = RecordBatch::try_from_iter([(
+            "BOOLS",
+            Arc::new(BooleanArray::from(vec![true, false])) as ArrayRef,
+        )])
+        .unwrap();
+
+        let alloc = Bump::new();
+        let table_ref = TableRef::from_str("test.table").unwrap();
+        let table_data_accessor =
+            TableDataAccessor::<TestScalar>::try_from_record_batch(&rb, 1, &alloc).unwrap();
+        let data_accessor_impl = DataAccessorImpl::new(
+            [(table_ref.clone(), table_data_accessor)]
+                .into_iter()
+                .collect(),
+        );
+
+        assert_eq!(data_accessor_impl.get_length(&table_ref), 1);
+        assert_eq!(data_accessor_impl.get_offset(&table_ref), 1);
+        assert_eq!(
+            data_accessor_impl.get_column(&table_ref, &Ident::new("BOOLS")),
+            Column::Boolean(&[true, false])
+        );
     }
 }
